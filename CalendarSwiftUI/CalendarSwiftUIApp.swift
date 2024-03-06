dd63a39a20f9651309ca67b4010a42c177949250
OlyaBlazhevich <154874492+OlyaBlazhevich@users.noreply.github.com>
2024-03-06 11:11:02 +0300

    Add files via upload

---
+++ CalendarSwiftUI/CalendarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarSwiftUIApp.swift
+//  CalendarSwiftUI
+//
+//  Created by Оля Блажевич on 1.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalendarView()
+        }
+    }
+}
